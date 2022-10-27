5af8fbd2c6ac22137b6e21fc12734bebb1d04577
Milo Ramirez <sinkpig00@gmail.com>
2022-10-27 19:40:54 -0300

    Added new users and adress column

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,14 @@ puts 'Deleting books and user'
 User.destroy_all
 Book.destroy_all
 puts 'End'
-puts 'Creating a user'
-user = User.create(name: "Juju", email: "test@test.com", password: "123123", password_confirmation: "123123")
+puts 'Creating a user 1'
+User.create(name: "Juju", email: "test@test.com", password: "123123", password_confirmation: "123123", address: "Rua 123")
+puts 'End'
+puts 'Creating a user 2'
+User.create(name: "Dani", email: "teste@teste.com", password: "123123", password_confirmation: "123123", address: "Rua 234")
+puts 'End'
+puts 'Creating a user 3'
+User.create(name: "Milo", email: "a@a.com", password: "123123", password_confirmation: "123123", address: "Rua 345")
 puts 'End'
 puts 'Creating 10 Books'
 10.times do
@@ -21,8 +27,20 @@ puts 'Creating 10 Books'
     author: Faker::Book.author,
     year: (1900..2022).to_a.sample,
     genre: %w[fantasy dystopian adventure romance horror thriller children].sample,
-    user_id: user.id
+    user_id: 1
   )
   book.save!
+end
+puts 'End'
+puts 'Creating 5 Books'
+5.times do
+  book2 = Book.new(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    year: (1900..2022).to_a.sample,
+    genre: %w[fantasy dystopian adventure romance horror thriller children].sample,
+    user_id: 3
+  )
+  book2.save!
 end
 puts 'End'
