c3378d62132ff90ba5a2a4237ad2db2bfb57d6b1
Vinicius Vatte de Lima <vvatte@yahoo.com>
2022-10-29 10:17:39 -0300

    esta funcionando o edit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
 
-  validates :title, :author, :year, :genre, :available, presence: true
+  validates :title, :author, :year, :genre, presence: true
   validates :year, numericality: { only_integer: true }
   validates :genre, inclusion: { in: %w[fantasy dystopian adventure romance horror thriller children] }
   # TODO: Upload image for a book
