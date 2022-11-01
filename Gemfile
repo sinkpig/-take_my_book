5a182463b0a4790d2102fe58510f497b3f5265c0
Daniela Rios <danirioscampelo@gmail.com>
2022-11-01 20:23:11 -0300

    search bar implemented

--- Gemfile
+++ Gemfile
@@ -55,6 +55,9 @@ gem "sassc-rails"
 gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
+gem "cloudinary"
+gem "pg_search"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,9 @@ class Book < ApplicationRecord
   validates :year, numericality: { only_integer: true }
   validates :genre, inclusion: %w[fantasy dystopian adventure romance horror thriller children]
   # TODO: Upload image for a book
+
+  include PgSearch::Model
+  pg_search_scope :search_by_attributes,
+    against: [ :title, :author, :genre, :year ],
+    using: { tsearch: { prefix: true } }
 end
